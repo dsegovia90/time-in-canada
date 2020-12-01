b8643a22b9bd80e9e7b1a4dfc49a513c6502bd5c
Daniel Segovia Arce <segoarce90@gmail.com>
2020-11-30 17:37:54 -0800

    add eslint/prettier and fix issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import Document, {
-  Html, Main, NextScript,
-} from 'next/document';
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import React from 'react'
+import Document, { Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/core/styles'
 
 export default class MyDocument extends Document {
-  render() {
+  render(): React.ReactElement {
     return (
       <Html lang="en">
         <body>
@@ -13,27 +11,27 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
 // it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
-
   // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
 
-  ctx.renderPage = () => originalRenderPage({
-    enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-  });
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    })
 
-  const initialProps = await Document.getInitialProps(ctx);
+  const initialProps = await Document.getInitialProps(ctx)
 
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
     styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
-  };
-};
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Box, Container } from '@material-ui/core'
+
+export default function Home() {
+  return (
+    <Box>
+      <Container>Hello</Container>
+    </Box>
+  )
+}
